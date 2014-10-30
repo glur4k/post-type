19d645e623a4913966ac0f45a9762cb98c7240ff
Glur4k <tonon.san@gmail.com>
2014-10-30 22:09:53 +0100

    Templates anpassen

--- templates/taxonomy-rp_spieler_mannschaft.php
+++ templates/taxonomy-rp_spieler_mannschaft.php
@@ -3,11 +3,6 @@
  * Template fuer die Anzeige einer einzelnen Mannschaft
  */
 
-// TODO:
-// - Position des Spielers vor dem Namen anzeigen
-// - den Link in der Info dynamisch generieren (ID aus Datenbank)
-// - Die letzten Ergebnisse der XXX dynamisch generieren
-
 $headerSep = get_template_directory_uri() . "/images/content-header-image-sep.png";
 get_header(); ?>
   <section id="content" class="clearfix page-widh-sidebar">
@@ -39,14 +34,19 @@ get_header(); ?>
 
           $args = array(
             'post_type' => 'rp_spieler',
+            'meta_key'=> $mannschaft,
+            'orderby' => 'meta_value_num',
+            'order' => ASC,
+            'posts_per_page' => -1,
             'tax_query' => array(
               array(
                 'taxonomy' => 'rp_spieler_mannschaft',
-                'field'    => 'slug',
-                'terms'    => $mannschaft
-              ),
-            ),
+                'field' => 'slug',
+                'terms' => $mannschaft
+              )
+            )
           );
+
           $the_query = new WP_Query($args);
         ?>
 
@@ -54,8 +54,6 @@ get_header(); ?>
           <!-- the loop -->
           <?php $count = 1; $last = false; ?>
           <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
-
 
             <?php if ($count === 3) {
               $last = true;
@@ -66,8 +64,6 @@ get_header(); ?>
             }?>
 
             <?php echo '<div class="dt-onethird' . ($last ? ' dt-onethirdlast' : '') . '">'; ?>
-
-
               <div class="rp_spieler">
                 <div class="dt-message dt-message-paragraph" style="text-align: center;">
 
@@ -77,6 +73,7 @@ get_header(); ?>
                   <a title="Ausf&uuml;hliche Statistiken zu <?php the_title(); ?>" class="spieler-portrait-wrapper wrapper-no-img" href="<?php echo get_post_permalink(); ?>"><?php echo ParserUtils::baueTitelKuerzel(get_the_title()); ?></a>
                 <?php endif; ?>
 
+                <h6><?php echo get_post_meta(get_the_ID(), $mannschaft)[0] ?></h6>
                 <h4><?php echo str_replace (" " , "<br>" , get_the_title()); ?></h4>
                 <table>
                   <tr>
@@ -93,41 +90,35 @@ get_header(); ?>
                 </div>
               </div>
             </div>
-
-
-
-
-
           <?php endwhile; ?>
           <!-- end of the loop -->
 
           <?php // Mannschafts Tabellen ausgeben ?>
           <div class="clearfix"></div>
           <div class="dt-separator-top post-spieler clearfix"><a class="scroll" href="#website-header">TOP</a></div>
-          <h5 class="dt-message dt-message-info">Die aktuelle Tabelle</h5>
           <?php
+            // Hole data_tabelle, data_ergebnisse, Link aus mannschaften_daten Tabelle
             $table_name = $wpdb->prefix . 'rp_mannschaften_daten';
-            $sql = "SELECT data_tabelle FROM $table_name
+            $sql = "SELECT data_tabelle, data_ergebnisse, link FROM $table_name
                     WHERE name = %s";
             $meta = $wpdb->get_row($wpdb->prepare($sql, ParserUtils::konvertiereMannschaftsNamen($mannschaft)), ARRAY_A);
-            echo implode('', $meta);
+            extract($meta);
           ?>
+          <h5 class="dt-message dt-message-info">Die aktuelle Tabelle</h5>
+          <?php echo $data_tabelle ?>
 
-          <h5 class="dt-message dt-message-info">Die letzten Ergebnisse der Herren</h5>
-          <?php
-            $sql = "SELECT data_ergebnisse FROM $table_name
-                    WHERE name = %s";
-            $meta = $wpdb->get_row($wpdb->prepare($sql, ParserUtils::konvertiereMannschaftsNamen($mannschaft)), ARRAY_A);
-            echo implode('', $meta);
-          ?>
+          <h5 class="dt-message dt-message-info">Die letzten Ergebnisse der <?php echo ParserUtils::konvertiereMannschaftsNamen($mannschaft) ?></h5>
+          <?php echo $data_ergebnisse ?>
 
           <?php wp_reset_postdata(); ?>
         <?php else : ?>
           <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
         <?php endif; ?>
 
-        <div class="dt-message dt-message-notice" style="text-align: center;"><span style="font-size: xx-small;">Die Informationen auf dieser Seite wurden bereitgestellt von Click-TT. Wir übernehmen keine Haftung für die Korrektheit der Daten.</span><br>
-Für weitere Informationen besuchen Sie bitte die Seite von <a href="http://ttvbw.click-tt.de/cgi-bin/WebObjects/nuLigaTTDE.woa/wa/clubTeams?club=1474" target="_blank">Click-TT</a>.</div>
+        <div class="dt-message dt-message-notice" style="text-align: center;"><span style="font-size: xx-small;">
+          Die Informationen auf dieser Seite wurden bereitgestellt von Click-TT. Wir &uuml;bernehmen keine Haftung für die Korrektheit der Daten.</span><br>
+          F&uuml;r weitere Informationen besuchen Sie bitte die Seite von <a href="<?php echo $link ?>" target="_blank">Click-TT</a>.
+        </div>
       </div>
     <?php wp_reset_postdata(); ?>
     <?php get_sidebar(); ?>
